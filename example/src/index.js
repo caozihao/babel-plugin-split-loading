0c380c2c7838819afc84892761030ef461647b03
caozihao <1154791107@qq.com>
2019-02-26 15:37:03 +0800

    项目初始化

---
+++ example/src/index.js
@@ -0,0 +1,4 @@
+import {flatten,join} from 'lodash'
+
+// import flatten from 'lodash/flatten'
+// import join from 'lodash/join'
---
+++ index.js
@@ -0,0 +1,37 @@
+
+const babel = require("@babel/core");
+
+module.exports = function ({ types: t }) {
+  return {
+    visitor: {
+      /**
+       * @param {*} path  当前节点的位置
+       * @param {*} state  插件选项
+       * 在babel.plugins里面配置
+      *   "plugins": [
+                [
+                  "demand-loading",
+                  // 这个对象就是state
+                    {
+                      "library": "lodash", 
+                      "idNeed": true
+                    }
+                ]
+            ]
+       */
+      ImportDeclaration(path, state) {
+        let { opts } = state
+        let node = path.node;
+        let specifiers = node.specifiers
+        if (opts.library == node.source.value ) {
+          //  console.log('specifiers ->', specifiers);
+          let newImport = specifiers.map((specifier) => {
+            return t.importDeclaration([t.ImportDefaultSpecifier(specifier.local)], t.stringLiteral(`${node.source.value}/${specifier.local.name}`))
+          });
+          // console.log('newImport ->', newImport);
+          path.replaceWithMultiple(newImport)
+        }
+      }
+    }
+  }
+}
